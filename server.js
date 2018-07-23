fc2776093a7e2970529a2906ff645f05360fe337
Yirklum <yirklum@aol.com>
2018-07-23 16:31:13 -0400

    Added route

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send("Hello World!");
-});
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
+
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/nyt-articles",
+    {
+      useMongoClient: true
+    }
+  );
+  
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT);
